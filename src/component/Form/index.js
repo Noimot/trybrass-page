c82e830f253ea25b07086dc0c7cce2b778797268
alabi noimot <kikkyal@gmail.com>
2021-04-22 11:44:28 +0100

    changes updated

--- src/component/Form/index.js
+++ src/component/Form/index.js
@@ -6,33 +6,52 @@ import './index.css'
 
 const Form = () => {
 
-const [person, setPerson] = useState ({
-    email: '',
-    password: ''
-})
+    const [userInfo, setUserInfo] = useState({
+        email: '',
+        password: ''
+    })
 
-// const emailRegex = 
+    const [formErrors, setformErrors] = useState({
+        email: '',
+        password: ''
+    })
 
-const handleChange = (e) => {
-    const name= e.target.name;
-    const value = e.target.value;
-    setPerson({...person, [name]:value})
-}
+    const emailvalidate = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]+$/gi;
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setUserInfo({ ...userInfo, [name]: value })
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(person.email,person.password)
-    
-    
-}
+        setformErrors(formErrors);
+        switch (name) {
+            
+            case 'email':
+                formErrors.email = emailvalidate.test(value) ? 'You entered a valid Email Address' : 'Please enter a valid Email Address';
+                break;
+            case 'password':
+                formErrors.password = value.length === 6 ? '' : 'Password must be 6 characters long';
+                break;
+            default:
+                break;
+        }
+        
+    }
 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(userInfo.email, userInfo.password)
+
+    }
+   
     return (
         <div className='form-container'>
             <h1>Log in to your account</h1>
             <form className="login-form">
-                <input type='email' name='email' value={person.email} placeholder='Email Address' onChange={handleChange} />
-                <input className='input-position' type='password' name='password' value={person.password} placeholder='Enter password' onChange={handleChange} />
-            
+                {formErrors.email && <div className='inputValidation'>{formErrors.email}</div>}
+                <input type='email' name='email' value={userInfo.email} placeholder='Email Address' onChange={handleChange} />
+                {formErrors.password && <div className='inputValidation'>{formErrors.password}</div>}
+                <input className='input-position' type='password' name='password' value={userInfo.password} placeholder='Enter password' onChange={handleChange} />
                 <button onClick={handleSubmit}><Link to='/dashboard'>Log me in</Link></button>
             </form>
             <p>Forgot your password? <Link to='#'>Reset it here</Link></p>
